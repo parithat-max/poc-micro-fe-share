50e3f110f746ebb67b1d77725e7f0d9967f8e3e2
Parithat <parithat@RBWGMAC014.local>
2026-06-17 13:18:03 +0700

    feat: add ModuleAPage component and re-export from share

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export * from "./ui";
-export * from "./store";
+export * from "./pages";
---
+++ src/pages/ModuleAPage.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Button } from '../ui';
+
+export default function ModuleAPage() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button appName="module-a" className="bg-amber-900 px-3 py-1">
+        Hello from Module A
+      </Button>
+    </div>
+  );
+}
